cab998ad24322d1a84d52a1c2e0008fd8a9c86d7
Emmanuel Keller <ekeller@open-search-server.com>
2009-05-14 13:37:26 +0000

    Fixed bad copyright label (Jaeksoft WebSearch -> Jaeksoft OpenSearchServer)

--- src/java/com/jaeksoft/searchlib/web/controller/crawler/web/CrawlController.java
+++ src/java/com/jaeksoft/searchlib/web/controller/crawler/web/CrawlController.java
@@ -1,24 +1,24 @@
 /**   
- * License Agreement for Jaeksoft WebSearch
+ * License Agreement for Jaeksoft OpenSearchServer
  *
  * Copyright (C) 2008 Emmanuel Keller / Jaeksoft
  * 
  * http://www.jaeksoft.com
  * 
- * This file is part of Jaeksoft WebSearch.
+ * This file is part of Jaeksoft OpenSearchServer.
  *
- * Jaeksoft WebSearch is free software: you can redistribute it and/or modify
+ * Jaeksoft OpenSearchServer is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- * Jaeksoft WebSearch is distributed in the hope that it will be useful,
+ * Jaeksoft OpenSearchServer is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with Jaeksoft WebSearch.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with Jaeksoft OpenSearchServer.  If not, see <http://www.gnu.org/licenses/>.
  **/
 
 package com.jaeksoft.searchlib.web.controller.crawler.web;
