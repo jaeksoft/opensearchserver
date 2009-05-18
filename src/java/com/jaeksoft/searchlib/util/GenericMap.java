377cac09dda86bda35854110d26dfd1a61ceef2e
Emmanuel Keller <ekeller@open-search-server.com>
2009-05-18 17:36:16 +0000

    - Added Map / Link generic class. - Starting mapping interface for web crawler  (logic not yet implemented)

---
+++ src/java/com/jaeksoft/searchlib/util/GenericMap.java
@@ -0,0 +1,69 @@
+/**   
+ * License Agreement for Jaeksoft OpenSearchServer
+ *
+ * Copyright (C) 2008 Emmanuel Keller / Jaeksoft
+ * 
+ * http://www.jaeksoft.com
+ * 
+ * This file is part of Jaeksoft OpenSearchServer.
+ *
+ * Jaeksoft OpenSearchServer is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * Jaeksoft OpenSearchServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Jaeksoft OpenSearchServer. 
+ *  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+package com.jaeksoft.searchlib.util;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+public class GenericMap<T> {
+
+	private TreeMap<T, List<T>> map;
+	private List<GenericLink<T>> list;
+
+	public GenericMap() {
+		map = new TreeMap<T, List<T>>();
+	}
+
+	public List<GenericLink<T>> getList() {
+		synchronized (this) {
+			if (list != null)
+				return list;
+			list = new ArrayList<GenericLink<T>>(0);
+			Iterator<Entry<T, List<T>>> it = map.entrySet().iterator();
+			while (it.hasNext()) {
+				Entry<T, List<T>> entry = it.next();
+				for (T t : entry.getValue())
+					list.add(new GenericLink<T>(entry.getKey(), t));
+			}
+			return list;
+		}
+	}
+
+	public void add(T source, T target) {
+		synchronized (this) {
+			List<T> l = map.get(source);
+			if (l == null) {
+				l = new ArrayList<T>();
+				map.put(source, l);
+			} else if (l.contains(target))
+				return;
+			l.add(target);
+			list = null;
+		}
+	}
+}
---
+++ src/java/com/jaeksoft/searchlib/web/controller/crawler/web/MappingController.java
@@ -0,0 +1,107 @@
+/**   
+ * License Agreement for Jaeksoft OpenSearchServer
+ *
+ * Copyright (C) 2008 Emmanuel Keller / Jaeksoft
+ * 
+ * http://www.jaeksoft.com
+ * 
+ * This file is part of Jaeksoft OpenSearchServer.
+ *
+ * Jaeksoft OpenSearchServer is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * Jaeksoft OpenSearchServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Jaeksoft OpenSearchServer.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+package com.jaeksoft.searchlib.web.controller.crawler.web;
+
+import java.util.List;
+
+import com.jaeksoft.searchlib.SearchLibException;
+import com.jaeksoft.searchlib.schema.SchemaField;
+import com.jaeksoft.searchlib.util.GenericMap;
+import com.jaeksoft.searchlib.web.controller.CommonController;
+
+public class MappingController extends CommonController {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4912079093808208146L;
+
+	private SchemaField selectedUrlField;
+
+	private SchemaField selectedIndexField;
+
+	private GenericMap<String> fieldMap;
+
+	public MappingController() throws SearchLibException {
+		super();
+		selectedUrlField = null;
+		selectedIndexField = null;
+		fieldMap = new GenericMap<String>();
+	}
+
+	public List<SchemaField> getUrlFieldList() throws SearchLibException {
+		synchronized (this) {
+			List<SchemaField> list = getClient().getUrlManager().getClient()
+					.getSchema().getFieldList().getList();
+			if (list.size() > 0 && selectedUrlField == null)
+				selectedUrlField = list.get(0);
+			return list;
+		}
+	}
+
+	public List<SchemaField> getIndexFieldList() throws SearchLibException {
+		synchronized (this) {
+			List<SchemaField> list = getClient().getSchema().getFieldList()
+					.getList();
+			if (list.size() > 0 && selectedIndexField == null)
+				selectedIndexField = list.get(0);
+			return list;
+		}
+	}
+
+	public void setSelectedUrlField(SchemaField field) {
+		synchronized (this) {
+			selectedUrlField = field;
+		}
+	}
+
+	public SchemaField getSelectedUrlField() {
+		synchronized (this) {
+			return selectedUrlField;
+		}
+	}
+
+	public void setSelectedIndexField(SchemaField field) {
+		synchronized (this) {
+			selectedIndexField = field;
+		}
+	}
+
+	public SchemaField getSelectedIndexField() {
+		synchronized (this) {
+			return selectedIndexField;
+		}
+	}
+
+	public GenericMap<String> getFieldMap() {
+		return fieldMap;
+	}
+
+	public void onAdd() {
+		if (selectedUrlField == null || selectedIndexField == null)
+			return;
+		fieldMap.add(selectedUrlField.getName(), selectedIndexField.getName());
+		reloadPage();
+	}
+}
