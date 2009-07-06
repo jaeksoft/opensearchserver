26e7ad6b5b9f44f93f42f42f533e4a94844ccc5e
Emmanuel Gosse <egosse@open-search-server.com>
2009-07-06 20:49:49 +0000

    file browser commit before computer crash

---
+++ src/java/com/jaeksoft/searchlib/template/FileCrawler.java
@@ -0,0 +1,42 @@
+/**   
+ * License Agreement for Jaeksoft OpenSearchServer
+ *
+ * Copyright (C) 2008-2009 Emmanuel Keller / Jaeksoft
+ * 
+ * http://www.open-search-server.com
+ * 
+ * This file is part of Jaeksoft OpenSearchServer.
+ *
+ * Jaeksoft OpenSearchServer is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * Jaeksoft OpenSearchServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Jaeksoft OpenSearchServer. 
+ *  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+package com.jaeksoft.searchlib.template;
+
+public class FileCrawler extends TemplateAbstract {
+
+	public final static String publicName = "file crawler";
+
+	public final static String description = "Index with predefined field, analyzers, parsers. Ready for web site crawl and indexation";
+
+	public final static String root = "/file_crawler";
+
+	public final static String[] resources = { "config.xml", "parsers.xml",
+			"requests.xml", "filecrawler-mapping.xml" };
+
+	protected FileCrawler() {
+		super(root, resources, publicName, description);
+	}
+
+}
