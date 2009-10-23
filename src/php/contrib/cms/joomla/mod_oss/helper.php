93c6330c21a5564b096beca7f4814153d981f442
Emmanuel Gosse <egosse@open-search-server.com>
2009-10-23 16:03:35 +0000

    version 0.01 of joomla module for Open Search Server use

---
+++ src/php/contrib/cms/joomla/mod_oss/helper.php
@@ -0,0 +1,9 @@
+<?php
+
+
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+
+class modSearchHelper {
+   
+}
---
+++ src/php/contrib/cms/joomla/mod_oss/tmpl/default.php
@@ -0,0 +1,10 @@
+<?php
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+include 'OSS_search.php';
+include 'OSS_list.php';
+include 'OSS_paging.php';
+?>
+
+<br/><br/><br/><br/>
+<div class="info" ><?php echo $params->get('endMessage') ?></div>
