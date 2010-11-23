79ae575021451ac89578b8c56a7a83bb210701ab
Emmanuel Keller <ekeller@open-search-server.com>
2010-11-23 16:55:21 +0000

    Work in progress on ASP example

--- examples/asp/App_Code/opensearchserver_paging.vb
+++ examples/asp/App_Code/opensearchserver_paging.vb
@@ -5,8 +5,10 @@ Namespace OpenSearchServer
 
     Public Class Paging
 
-        Private totalPages As Integer
-        Private currentPage As Integer
+        Private totalPages As Long
+        Private currentPage As Long
+        Private leftPage As Long
+        Private rightPage As Long
 
         Public Sub New(ByRef result As Result, ByVal maxPages As Integer)
             Dim numFound As Long = result.getNumFound
@@ -14,10 +16,22 @@ Namespace OpenSearchServer
             Dim start As Long = result.getStart
             totalPages = 0
             currentPage = 0
+            leftPage = 0
+            rightPage = 0
             If numFound > 0 Then
                 totalPages = (numFound + rows - 1) \ rows
                 currentPage = (start + rows) \ rows
+                leftPage = currentPage - maxPages \ 2
+                If (leftPage < 1) Then leftPage = 1
+                rightPage = leftPage + maxPages - 1
+                If (rightPage > totalPages) Then
+                    rightPage = totalPages
+                    leftPage = rightPage - maxPages + 1
+                    If (leftPage < 1) Then leftPage = 1
+                End If
+
             End If
+
 
         End Sub
 
@@ -27,6 +41,49 @@ Namespace OpenSearchServer
 
         Public Function getCurrentPage() As Long
             Return currentPage
+        End Function
+
+        Public Function getLeftPage() As Long
+            Return leftPage
+        End Function
+
+        Public Function getRightPage() As Long
+            Return leftPage
+        End Function
+
+        Public Function getRenderAsLi(ByRef classForPage As String, ByRef classForCurrentPage As String, ByRef currentUrl As String, ByRef pageParameter As String) As String
+            Dim sb As StringBuilder = New StringBuilder()
+            If (leftPage = 0) Then Return Nothing
+            For i As Long = leftPage To rightPage
+                sb.Append("<li")
+                If (i = currentPage) Then
+                    If (classForCurrentPage <> Nothing) Then
+                        sb.Append(" class=""")
+                        sb.Append(classForCurrentPage)
+                        sb.Append("""")
+                    End If
+                ElseIf (classForPage <> Nothing) Then
+                    sb.Append(" class=""")
+                    sb.Append(classForPage)
+                    sb.Append("""")
+                End If
+                sb.Append(">")
+                sb.Append("&nbsp;")
+                If (currentUrl <> Nothing) Then
+                    sb.Append("<a href=""")
+                    sb.Append(currentUrl)
+                    sb.Append("&")
+                    sb.Append(pageParameter)
+                    sb.Append("=")
+                    sb.Append(i.ToString)
+                    sb.Append(""">")
+                End If
+                sb.Append(i.ToString)
+                If (currentUrl <> Nothing) Then sb.Append("</a>")
+                sb.Append("&nbsp;")
+                sb.Append("</li>")
+            Next
+            Return sb.ToString
         End Function
 
     End Class
