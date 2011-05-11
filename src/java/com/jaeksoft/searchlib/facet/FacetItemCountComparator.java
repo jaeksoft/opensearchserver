0ab681d6cad826c30925e5f0602ca77bace739db
Emmanuel Keller <ekeller@open-search-server.com>
2011-05-11 07:44:02 +0000

    Adding the facetItemCountComparator to be able to sort the facet items

---
+++ src/java/com/jaeksoft/searchlib/facet/FacetItemCountComparator.java
@@ -0,0 +1,40 @@
+/**   
+ * License Agreement for Jaeksoft OpenSearchServer
+ *
+ * Copyright (C) 2011 Emmanuel Keller / Jaeksoft
+ * 
+ * http://www.open-search-server.com
+ * 
+ * This file is part of Jaeksoft OpenSearchServer.
+ *
+ * Jaeksoft OpenSearchServer is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * Jaeksoft OpenSearchServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with Jaeksoft OpenSearchServer. 
+ *  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+package com.jaeksoft.searchlib.facet;
+
+import java.util.Comparator;
+
+public class FacetItemCountComparator implements Comparator<FacetItem> {
+
+	@Override
+	final public int compare(FacetItem item1, FacetItem item2) {
+		if (item1.count < item2.count)
+			return -1;
+		if (item1.count > item2.count)
+			return 1;
+		return 0;
+	}
+
+}
