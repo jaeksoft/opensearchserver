680b876a7c1221e88153463cf93ef6d532308c9a
Naveen.A.N <naveen.ann@open-search-server.com>
2012-01-18 15:24:53 +0000

    New feature 3475319: Autocompletion

--- src/java/com/jaeksoft/searchlib/renderer/Renderer.java
+++ src/java/com/jaeksoft/searchlib/renderer/Renderer.java
@@ -1,7 +1,7 @@
 /**   
  * License Agreement for OpenSearchServer
  *
- * Copyright (C) 2012 Emmanuel Keller / Jaeksoft
+ * Copyright (C) 2011-2012 Emmanuel Keller / Jaeksoft
  * 
  * http://www.open-search-server.com
  * 
