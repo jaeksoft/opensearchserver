31c28703b6b993922ce854053ca942f4c8b1de83
Emmanuel Keller <ekeller@open-search-server.com>
2012-02-03 12:47:26 +0000

    Reduce the number of line visible (10000) in the log viewer

--- src/java/com/jaeksoft/searchlib/web/controller/runtime/LogsController.java
+++ src/java/com/jaeksoft/searchlib/web/controller/runtime/LogsController.java
@@ -90,7 +90,7 @@ public class LogsController extends CommonController {
 
 	public String getCurrentLog() throws IOException {
 		if (currentLog == null && selectedFile != null)
-			currentLog = Logging.readLogs(100000, selectedFile);
+			currentLog = Logging.readLogs(10000, selectedFile);
 		return currentLog;
 	}
 
