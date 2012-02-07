8111cf5f21caa22caf2c945964ab68331212e933
Emmanuel Keller <ekeller@open-search-server.com>
2012-02-07 09:58:00 +0000

    Bug fix 3485241: Pages with meta noindex are indexed

--- src/java/com/jaeksoft/searchlib/crawler/web/spider/Crawl.java
+++ src/java/com/jaeksoft/searchlib/crawler/web/spider/Crawl.java
@@ -150,6 +150,7 @@ public class Crawl {
 		parser.setSourceDocument(sourceDocument);
 		Date parserStartDate = new Date();
 		parser.parseContent(inputStream);
+
 		urlItem.clearInLinks();
 		urlItem.addInLinks(parser
 				.getFieldContent(ParserFieldEnum.internal_link));
@@ -180,6 +181,14 @@ public class Crawl {
 		}
 		if (newLastModifiedTime != null)
 			urlItem.setLastModifiedDate(newLastModifiedTime);
+
+		for (FieldValueItem item : parser.getFieldContent(
+				ParserFieldEnum.meta_robots).getValues())
+			if ("noindex".equalsIgnoreCase(item.getValue())) {
+				urlItem.setIndexStatus(IndexStatus.META_NOINDEX);
+				break;
+			}
+
 		this.parser = parser;
 	}
 
