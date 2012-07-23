a67ea68917efce834bee28a94431af12c9834b95
Emmanuel Keller <ekeller@open-search-server.com>
2012-07-23 14:00:42 +0000

    Youtube fixes

--- src/java/com/jaeksoft/searchlib/util/video/YouTube.java
+++ src/java/com/jaeksoft/searchlib/util/video/YouTube.java
@@ -25,9 +25,12 @@ package com.jaeksoft.searchlib.util.video;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
@@ -64,15 +67,30 @@ public class YouTube {
 		return youtubeItem;
 	}
 
+	private final static Pattern[] idPatterns = {
+			Pattern.compile("/embed/([^/]*)"), Pattern.compile("/v/([^/]*)") };
+
 	/*
 	 * This method is to extract the Video id from youtube urls like
 	 * http://www.youtube.com/watch?v=asdoss-1
+	 * http://www.youtube.com/v/Ahg6qcgoay4
 	 */
 	private static String getVideoId(URL url) throws URISyntaxException {
-		List<NameValuePair> pairs = URLEncodedUtils.parse(url.toURI(), "UTF-8");
+		URI uri = url.toURI();
+		// Checking v=
+		List<NameValuePair> pairs = URLEncodedUtils.parse(uri, "UTF-8");
 		for (NameValuePair pair : pairs)
 			if ("v".equals(pair.getName()))
 				return pair.getValue();
+		// Checking on path
+		String path = uri.getPath();
+		for (Pattern pattern : idPatterns) {
+			synchronized (pattern) {
+				Matcher urlMatcher = pattern.matcher(path);
+				if (urlMatcher.matches())
+					return urlMatcher.group(1);
+			}
+		}
 		return null;
 	}
 
