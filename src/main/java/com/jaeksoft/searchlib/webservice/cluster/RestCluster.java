9af2b9d68c24850c8d699d4b04e63a4728b8cacd
Emmanuel Keller <ekeller@open-search-server.com>
2013-12-21 14:15:37 +0100

    #325 implements info method of Cluster WebService

---
+++ src/main/java/com/jaeksoft/searchlib/webservice/cluster/RestCluster.java
@@ -0,0 +1,40 @@
+/**   
+ * License Agreement for OpenSearchServer
+ *
+ * Copyright (C) 2013 Emmanuel Keller / Jaeksoft
+ * 
+ * http://www.open-search-server.com
+ * 
+ * This file is part of OpenSearchServer.
+ *
+ * OpenSearchServer is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ * OpenSearchServer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenSearchServer. 
+ *  If not, see <http://www.gnu.org/licenses/>.
+ **/
+package com.jaeksoft.searchlib.webservice.cluster;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/cluster")
+public interface RestCluster {
+
+	@GET
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public ClusterInfoResult info(@QueryParam("login") String login,
+			@QueryParam("key") String key);
+
+}
