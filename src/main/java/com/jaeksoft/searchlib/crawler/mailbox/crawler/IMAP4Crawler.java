20e9515543c027eab78d29cb41d845a2fe07314d
Emmanuel Keller <ekeller@open-search-server.com>
2014-08-18 09:53:13 -0500

    Fix imap4 crawl issue

--- src/main/java/com/jaeksoft/searchlib/crawler/mailbox/crawler/IMAP4Crawler.java
+++ src/main/java/com/jaeksoft/searchlib/crawler/mailbox/crawler/IMAP4Crawler.java
@@ -33,7 +33,6 @@ import javax.mail.MessagingException;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 
-import com.jaeksoft.searchlib.Logging;
 import com.jaeksoft.searchlib.SearchLibException;
 import com.jaeksoft.searchlib.crawler.mailbox.MailboxFieldEnum;
 import com.jaeksoft.searchlib.crawler.mailbox.MailboxProtocolEnum;
@@ -64,21 +63,18 @@ public class IMAP4Crawler extends MailboxAbstractCrawler {
 		IMAPStore store = (IMAPStore) session.getStore(storeProtocol);
 		try {
 			store.connect();
-			logFolder("PERSONAL NAMESPACES: ", store.getPersonalNamespaces());
-			logFolder("SHARED NAMESPACES: ", store.getSharedNamespaces());
 			Folder defaultFolder = store.getDefaultFolder();
-			Logging.info("DEFAULT FOLDER: " + defaultFolder.getFullName());
-			readFolder(defaultFolder);
+			if (defaultFolder == null)
+				return;
+			Folder[] folders = defaultFolder.list();
+			if (folders == null)
+				return;
+			for (Folder folder : folders)
+				readFolder(folder);
 		} finally {
 			if (store != null)
 				store.close();
 		}
-	}
-
-	private void logFolder(String source, Folder[] folders) {
-		Logging.info("SOURCE FOLDERS: " + source);
-		for (Folder folder : folders)
-			Logging.info(folder.getFullName());
 	}
 
 	@Override
