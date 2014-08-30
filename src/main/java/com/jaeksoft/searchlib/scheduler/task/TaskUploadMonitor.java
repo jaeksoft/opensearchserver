7bf665c8d2f8753f6d4a7dff826cd8bbc8d2e0bc
Emmanuel Keller <ekeller@open-search-server.com>
2014-08-30 13:28:43 +0200

    Update Monitoring task

--- src/main/java/com/jaeksoft/searchlib/scheduler/task/TaskUploadMonitor.java
+++ src/main/java/com/jaeksoft/searchlib/scheduler/task/TaskUploadMonitor.java
@@ -25,39 +25,28 @@
 package com.jaeksoft.searchlib.scheduler.task;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
-import org.apache.http.StatusLine;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.params.HttpClientParams;
-import org.apache.http.conn.ClientConnectionManager;
-import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.entity.mime.content.StringBody;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParamBean;
-import org.apache.http.util.EntityUtils;
 
 import com.jaeksoft.searchlib.Client;
 import com.jaeksoft.searchlib.Monitor;
 import com.jaeksoft.searchlib.SearchLibException;
 import com.jaeksoft.searchlib.config.Config;
+import com.jaeksoft.searchlib.crawler.web.database.CredentialItem;
+import com.jaeksoft.searchlib.crawler.web.database.CredentialItem.CredentialType;
+import com.jaeksoft.searchlib.crawler.web.spider.DownloadItem;
+import com.jaeksoft.searchlib.crawler.web.spider.HttpDownloader;
 import com.jaeksoft.searchlib.scheduler.TaskAbstract;
 import com.jaeksoft.searchlib.scheduler.TaskLog;
 import com.jaeksoft.searchlib.scheduler.TaskProperties;
 import com.jaeksoft.searchlib.scheduler.TaskPropertyDef;
 import com.jaeksoft.searchlib.scheduler.TaskPropertyType;
-import com.jaeksoft.searchlib.util.IOUtils;
 import com.jaeksoft.searchlib.util.Variables;
 
 public class TaskUploadMonitor extends TaskAbstract {
@@ -97,7 +86,7 @@ public class TaskUploadMonitor extends TaskAbstract {
 	@Override
 	public String getDefaultValue(Config config, TaskPropertyDef propertyDef) {
 		if (propertyDef == propUrl)
-			return "http://www.open-search-server.com/oss-monitor/";
+			return "http://www.opensearchserver.com/oss-monitor/";
 		return null;
 	}
 
@@ -114,51 +103,36 @@ public class TaskUploadMonitor extends TaskAbstract {
 		String login = properties.getValue(propLogin);
 		String password = properties.getValue(propPassword);
 		String instanceId = properties.getValue(propInstanceId);
-		HttpParams params = new BasicHttpParams();
-		HttpProtocolParamBean paramsBean = new HttpProtocolParamBean(params);
-		paramsBean.setVersion(HttpVersion.HTTP_1_1);
-		paramsBean.setContentCharset("UTF-8");
-		HttpClientParams.setRedirecting(params, true);
-		DefaultHttpClient httpClient = new DefaultHttpClient(params);
-		CredentialsProvider credential = httpClient.getCredentialsProvider();
-		if (login != null && login.length() > 0 && password != null
-				&& password.length() > 0)
-			credential.setCredentials(
-					new AuthScope(uri.getHost(), uri.getPort()),
-					new UsernamePasswordCredentials(login, password));
-		else
-			credential.clear();
 
-		HttpPost httpPost = new HttpPost(uri);
-		MultipartEntity reqEntity = new MultipartEntity();
-		new Monitor().writeToPost(reqEntity);
+		CredentialItem credentialItem = null;
+		if (!StringUtils.isEmpty(login) && !StringUtils.isEmpty(password))
+			credentialItem = new CredentialItem(CredentialType.BASIC_DIGEST,
+					null, login, password, null, null);
+		HttpDownloader downloader = client.getWebCrawlMaster()
+				.getNewHttpDownloader(true);
 		try {
-			reqEntity.addPart("instanceId", new StringBody(instanceId));
-		} catch (UnsupportedEncodingException e) {
-			IOUtils.close(httpClient);
-			throw new SearchLibException(e);
-		}
-		httpPost.setEntity(reqEntity);
-		try {
-			HttpResponse httpResponse = httpClient.execute(httpPost);
-			HttpEntity resEntity = httpResponse.getEntity();
-			StatusLine statusLine = httpResponse.getStatusLine();
-			EntityUtils.consume(resEntity);
-			if (statusLine.getStatusCode() != 200)
+			MultipartEntityBuilder entityBuilder = MultipartEntityBuilder
+					.create().addPart("instanceId",
+							new StringBody(instanceId, ContentType.TEXT_PLAIN));
+
+			new Monitor().writeToPost(entityBuilder);
+			DownloadItem downloadItem = downloader.post(uri, credentialItem,
+					null, null, entityBuilder.build());
+			if (downloadItem.getStatusCode() != 200)
 				throw new SearchLibException("Wrong code status:"
-						+ statusLine.getStatusCode() + " "
-						+ statusLine.getReasonPhrase());
+						+ downloadItem.getStatusCode() + " "
+						+ downloadItem.getReasonPhrase());
 			taskLog.setInfo("Monitoring data uploaded");
 		} catch (ClientProtocolException e) {
 			throw new SearchLibException(e);
 		} catch (IOException e) {
 			throw new SearchLibException(e);
+		} catch (IllegalStateException e) {
+			throw new SearchLibException(e);
+		} catch (URISyntaxException e) {
+			throw new SearchLibException(e);
 		} finally {
-			ClientConnectionManager connectionManager = httpClient
-					.getConnectionManager();
-			if (connectionManager != null)
-				connectionManager.shutdown();
-			IOUtils.close(httpClient);
+			downloader.release();
 		}
 
 	}
