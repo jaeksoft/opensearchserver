7c52f5464331178050257304bdfb1a5d52dce6b4
Emmanuel Keller <ekeller@open-search-server.com>
2014-08-31 18:00:58 +0200

    Fix #959, and add support of escape

--- src/main/java/com/jaeksoft/searchlib/query/parser/BooleanQueryBaseListener.java
+++ src/main/java/com/jaeksoft/searchlib/query/parser/BooleanQueryBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from com/jaeksoft/searchlib/query/parser/BooleanQuery.g4 by ANTLR 4.1
+// Generated from com/jaeksoft/searchlib/query/parser/BooleanQuery.g4 by ANTLR 4.3
 package com.jaeksoft.searchlib.query.parser;
 
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -14,39 +14,52 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 public class BooleanQueryBaseListener implements BooleanQueryListener {
 	/**
 	 * {@inheritDoc}
-	 * <p/>
-	 * The default implementation does nothing.
+	 *
+	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void enterExpression(@NotNull BooleanQueryParser.ExpressionContext ctx) { }
 	/**
 	 * {@inheritDoc}
-	 * <p/>
-	 * The default implementation does nothing.
+	 *
+	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void exitExpression(@NotNull BooleanQueryParser.ExpressionContext ctx) { }
 
 	/**
 	 * {@inheritDoc}
-	 * <p/>
-	 * The default implementation does nothing.
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterTerm(@NotNull BooleanQueryParser.TermContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitTerm(@NotNull BooleanQueryParser.TermContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void enterEveryRule(@NotNull ParserRuleContext ctx) { }
 	/**
 	 * {@inheritDoc}
-	 * <p/>
-	 * The default implementation does nothing.
+	 *
+	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void exitEveryRule(@NotNull ParserRuleContext ctx) { }
 	/**
 	 * {@inheritDoc}
-	 * <p/>
-	 * The default implementation does nothing.
+	 *
+	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void visitTerminal(@NotNull TerminalNode node) { }
 	/**
 	 * {@inheritDoc}
-	 * <p/>
-	 * The default implementation does nothing.
+	 *
+	 * <p>The default implementation does nothing.</p>
 	 */
 	@Override public void visitErrorNode(@NotNull ErrorNode node) { }
 }
