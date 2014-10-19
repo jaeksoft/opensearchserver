38c4d525c32cb9f132612c13a75178f07d2ddb7d
Emmanuel Keller <ekeller@open-search-server.com>
2014-10-19 12:25:37 +0200

    Implements #1105

--- src/main/java/com/jaeksoft/searchlib/scheduler/TaskEnum.java
+++ src/main/java/com/jaeksoft/searchlib/scheduler/TaskEnum.java
@@ -36,6 +36,7 @@ import com.jaeksoft.searchlib.scheduler.task.TaskFileManagerAction;
 import com.jaeksoft.searchlib.scheduler.task.TaskFlushCrawlCache;
 import com.jaeksoft.searchlib.scheduler.task.TaskFtpXmlFeed;
 import com.jaeksoft.searchlib.scheduler.task.TaskLearnerRun;
+import com.jaeksoft.searchlib.scheduler.task.TaskMailboxCrawlerRun;
 import com.jaeksoft.searchlib.scheduler.task.TaskMergeDataIndex;
 import com.jaeksoft.searchlib.scheduler.task.TaskOptimizeIndex;
 import com.jaeksoft.searchlib.scheduler.task.TaskOtherScheduler;
@@ -75,6 +76,8 @@ public class TaskEnum extends ExtensibleEnum<TaskEnumItem> {
 		new TaskEnumItem(this, TaskFileCrawlerStop.class);
 
 		new TaskEnumItem(this, TaskLearnerRun.class);
+
+		new TaskEnumItem(this, TaskMailboxCrawlerRun.class);
 
 		new TaskEnumItem(this, TaskMergeDataIndex.class);
 
